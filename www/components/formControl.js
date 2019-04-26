308c5ea66610847cc08fc4990087b91739861b35
slackbot115 <viniccius.carvalho22@gmail.com>
2019-04-26 11:39:57 +0900

    Adicionando estruturas e testando JS

---
+++ www/components/formControl.js
@@ -0,0 +1,17 @@
+$(document).on("click","#somar", function(){
+  var DS=0;
+  var EF=0;
+  var ADM=0;
+  var MN=0;
+  var ED=0;
+  var TR=0;
+
+
+    if(true==($("#p").val())){ 
+      $("#quadroPergunta").val(result);
+    }
+    
+    var valor2 = parseInt($("#valor2").val());
+    
+    $("#result").val("AEEE CARAMBA");
+});
