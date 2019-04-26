0e0c1c1a9bc1dc144e8a1a9f7b3ec895b9ffea13
FylipePablo <fylipe5201pablo@hotmail.com>
2019-04-26 17:22:43 +0900

    Projeto quase finalizado. Erros encontrados:Inscreva-se da aba cursos - Não abre o navegador quando é utilizado em apkQuiz - Ainda não esta funcionando

--- www/components/comandos.js
+++ www/components/comandos.js
@@ -1,2 +1,116 @@
 // This is a JavaScript file
 
+$(document).on('click','#lihome', function(){
+  $('#home').css({display:"block"});
+  $('#cursos').css({display:"none"});
+  $('#escola').css({display:"none"});
+});
+
+$(document).on('click','#liescola', function(){
+  $('#home').css({display:"none"});
+  $('#cursos').css({display:"none"});
+  $('#escola').css({display:"block"});
+});
+
+$(document).on('click','#licursos', function(){
+  var cont = 1;
+  $('#home').css({display:"none"});
+  $('#cursos').css({display:"block"});
+  $('#escola').css({display:"none"});
+
+  window.onload = function(e){ 
+
+  }
+  function retorno2(buttonIndex){
+      if(buttonIndex == 1){
+        navigator.notification.alert("Digitou OK");
+        $(document).on('click','#teste', function(){
+          $('#quiz').css({display:"block"});
+          $('#quiz2').css({display:"none"});
+          $('#quiz3').css({display:"none"});
+          $('#quiz4').css({display:"none"});
+          $('#quiz5').css({display:"none"});
+          $('#cursos').css({display:"none"});
+          $('#home').css({display:"none"});
+          $('#escola').css({display:"none"});
+        });
+      }
+
+      else{
+      }
+    }
+
+    $(document).on('click','#n3',function(){
+      navigator.notification.confirm("Deseja fazer o teste vocacional?", retorno2, "Janela", ["Sim","Não"]);
+    });
+});
+
+$(document).on('click','#insc', function(){
+  var ref = cordova.InAppBrowser.open('https://www.vestibulinhoetec.com.br/candidato/dados-acesso.asp', '_blank', 'location=yes');
+});
+
+$(document).on('click','#teste', function(){
+  $('#quiz').css({display:"block"});
+  $('#quiz2').css({display:"none"});
+  $('#quiz3').css({display:"none"});
+  $('#quiz4').css({display:"none"});
+  $('#quiz5').css({display:"none"});
+  $('#cursos').css({display:"none"});
+  $('#home').css({display:"none"});
+  $('#escola').css({display:"none"});
+});
+
+$(document).on('click','#butao1', function(){
+  $('#quiz').css({display:"none"});
+  $('#quiz2').css({display:"block"});
+  $('#quiz3').css({display:"none"});
+  $('#quiz4').css({display:"none"});
+  $('#quiz5').css({display:"none"});
+  $('#cursos').css({display:"none"});
+  $('#home').css({display:"none"});
+  $('#escola').css({display:"none"});
+});
+
+$(document).on('click','#butao2', function(){
+  $('#quiz').css({display:"none"});
+  $('#quiz2').css({display:"none"});
+  $('#quiz3').css({display:"block"});
+  $('#quiz4').css({display:"none"});
+  $('#quiz5').css({display:"none"});
+  $('#cursos').css({display:"none"});
+  $('#home').css({display:"none"});
+  $('#escola').css({display:"none"});
+});
+
+$(document).on('click','#butao3', function(){
+  $('#quiz').css({display:"none"});
+  $('#quiz2').css({display:"none"});
+  $('#quiz3').css({display:"none"});
+  $('#quiz4').css({display:"block"});
+  $('#quiz5').css({display:"none"});
+  $('#cursos').css({display:"none"});
+  $('#home').css({display:"none"});
+  $('#escola').css({display:"none"});
+});
+
+$(document).on('click','#butao4', function(){
+  $('#quiz').css({display:"none"});
+  $('#quiz2').css({display:"none"});
+  $('#quiz3').css({display:"none"});
+  $('#quiz4').css({display:"none"});
+  $('#quiz5').css({display:"block"});
+  $('#cursos').css({display:"none"});
+  $('#home').css({display:"none"});
+  $('#escola').css({display:"none"});
+});
+
+$(document).on('click','#butao5', function(){
+  $('#quiz').css({display:"none"});
+  $('#quiz2').css({display:"none"});
+  $('#quiz3').css({display:"none"});
+  $('#quiz4').css({display:"none"});
+  $('#quiz5').css({display:"none"});
+  $('#cursos').css({display:"block"});
+  $('#home').css({display:"none"});
+  $('#escola').css({display:"none"});
+});
